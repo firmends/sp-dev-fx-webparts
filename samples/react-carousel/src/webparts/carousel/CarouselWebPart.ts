35a8cdb8b74e4da12ea0175a1af1aaa64f7f844e
Simon Moon <firmends@yahoo.com>
2020-11-12 18:33:45 -0600

    prettier format

--- samples/react-carousel/src/webparts/carousel/CarouselWebPart.ts
+++ samples/react-carousel/src/webparts/carousel/CarouselWebPart.ts
@@ -1,24 +1,24 @@
 //import '@pnp/polyfill-ie11';
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField,
   PropertyPaneDropdown,
   IPropertyPaneDropdownOption,
   PropertyPaneLabel,
-  PropertyPaneToggle
-} from '@microsoft/sp-property-pane';
+  PropertyPaneToggle,
+} from "@microsoft/sp-property-pane";
 
-import * as strings from 'CarouselWebPartStrings';
-import Carousel from './components/Carousel';
-import { ICarouselProps } from './components/ICarouselProps';
-import spservices from '../../spservices/spservices';
-import { PropertyFieldNumber } from '@pnp/spfx-property-controls/lib/PropertyFieldNumber';
-import { ImageFit } from 'office-ui-fabric-react';
+import * as strings from "CarouselWebPartStrings";
+import Carousel from "./components/Carousel";
+import { ICarouselProps } from "./components/ICarouselProps";
+import spservices from "../../spservices/spservices";
+import { PropertyFieldNumber } from "@pnp/spfx-property-controls/lib/PropertyFieldNumber";
+import { ImageFit } from "office-ui-fabric-react";
 
 export interface ICarouselWebPartProps {
   title: string;
@@ -29,27 +29,29 @@ export interface ICarouselWebPartProps {
   includeCaption: boolean;
   sliderDelay: number;
   imageFitStyle: ImageFit;
-
 }
 
-export default class CarouselWebPart extends BaseClientSideWebPart<ICarouselWebPartProps> {
+export default class CarouselWebPart extends BaseClientSideWebPart<
+  ICarouselWebPartProps
+> {
   private lists: IPropertyPaneDropdownOption[] = [];
   private listsDropdownDisabled: boolean = true;
   private spService: spservices = null;
   private errorMessage: string;
 
-   // onInit
-   public  async onInit(): Promise<void> {
-
+  // onInit
+  public async onInit(): Promise<void> {
     this.spService = new spservices(this.context);
-    this.properties.siteUrl = this.properties.siteUrl ? this.properties.siteUrl : this.context.pageContext.site.absoluteUrl;
+    this.properties.siteUrl = this.properties.siteUrl
+      ? this.properties.siteUrl
+      : this.context.pageContext.site.absoluteUrl;
 
     if (this.properties.siteUrl && !this.properties.list) {
-     const _lists = await this.loadLists();
-     if ( _lists.length > 0 ){
-      this.lists = _lists;
-      this.properties.list = this.lists[0].key.toString();
-     }
+      const _lists = await this.loadLists();
+      if (_lists.length > 0) {
+        this.lists = _lists;
+        this.properties.list = this.lists[0].key.toString();
+      }
     }
     // default imageFitStyle option the first time web part is added to the page
     if (!this.properties.imageFitStyle) {
@@ -59,9 +61,7 @@ export default class CarouselWebPart extends BaseClientSideWebPart<ICarouselWebP
     return Promise.resolve();
   }
 
-
   protected async onPropertyPaneConfigurationStart() {
-
     try {
       if (this.properties.siteUrl) {
         const _lists = await this.loadLists();
@@ -69,38 +69,38 @@ export default class CarouselWebPart extends BaseClientSideWebPart<ICarouselWebP
         this.listsDropdownDisabled = false;
         //  await this.loadFields(this.properties.siteUrl);
         this.context.propertyPane.refresh();
-
       } else {
         this.lists = [];
-        this.properties.list = '';
+        this.properties.list = "";
         this.listsDropdownDisabled = false;
         this.context.propertyPane.refresh();
       }
-
-    } catch (error) {
-
-    }
+    } catch (error) {}
   }
 
   private async loadLists(): Promise<IPropertyPaneDropdownOption[]> {
     const _lists: IPropertyPaneDropdownOption[] = [];
     try {
-      const results = await this.spService.getSiteLists(this.properties.siteUrl);
+      const results = await this.spService.getSiteLists(
+        this.properties.siteUrl
+      );
       for (const list of results) {
         _lists.push({ key: list.Id, text: list.Title });
       }
       // push new item value
     } catch (error) {
-      this.errorMessage =  `${ escape(error.message.toString())} -  please check if site url if valid.` ;
+      this.errorMessage = `${escape(
+        error.message.toString()
+      )} -  please check if site url if valid.`;
       this.context.propertyPane.refresh();
     }
     return _lists;
   }
 
   private onSiteUrlGetErrorMessage(value: string) {
-    let returnValue: string = '';
+    let returnValue: string = "";
     if (value) {
-      returnValue = '';
+      returnValue = "";
     } else {
       const previousList: string = this.properties.list;
       const previousSiteUrl: string = this.properties.siteUrl;
@@ -109,43 +109,58 @@ export default class CarouselWebPart extends BaseClientSideWebPart<ICarouselWebP
       this.properties.siteUrl = undefined;
       this.lists = [];
       this.listsDropdownDisabled = true;
-      this.onPropertyPaneFieldChanged('list', previousList, this.properties.list);
-      this.onPropertyPaneFieldChanged('siteUrl', previousSiteUrl, this.properties.siteUrl);
+      this.onPropertyPaneFieldChanged(
+        "list",
+        previousList,
+        this.properties.list
+      );
+      this.onPropertyPaneFieldChanged(
+        "siteUrl",
+        previousSiteUrl,
+        this.properties.siteUrl
+      );
       this.context.propertyPane.refresh();
     }
     return returnValue;
   }
 
-  protected async onPropertyPaneFieldChanged(propertyPath: string, oldValue: string, newValue: string) {
+  protected async onPropertyPaneFieldChanged(
+    propertyPath: string,
+    oldValue: string,
+    newValue: string
+  ) {
     try {
       // reset any error
       this.properties.errorMessage = undefined;
       this.errorMessage = undefined;
       this.context.propertyPane.refresh();
 
-      if (propertyPath === 'siteUrl' && newValue) {
+      if (propertyPath === "siteUrl" && newValue) {
         super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
         const _oldValue = this.properties.list;
-        this.onPropertyPaneFieldChanged('list', _oldValue, this.properties.list);
+        this.onPropertyPaneFieldChanged(
+          "list",
+          _oldValue,
+          this.properties.list
+        );
         this.context.propertyPane.refresh();
         const _lists = await this.loadLists();
         this.lists = _lists;
         this.listsDropdownDisabled = false;
-        this.properties.list = this.lists.length > 0 ? this.lists[0].key.toString() : undefined;
+        this.properties.list =
+          this.lists.length > 0 ? this.lists[0].key.toString() : undefined;
         this.context.propertyPane.refresh();
         this.render();
-      }
-      else {
+      } else {
         super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
       }
     } catch (error) {
-      this.errorMessage =  `${error.message} -  please check if site url if valid.` ;
+      this.errorMessage = `${error.message} -  please check if site url if valid.`;
       this.context.propertyPane.refresh();
     }
   }
 
   public render(): void {
-
     const element: React.ReactElement<ICarouselProps> = React.createElement(
       Carousel,
       {
@@ -171,83 +186,78 @@ export default class CarouselWebPart extends BaseClientSideWebPart<ICarouselWebP
     ReactDom.unmountComponentAtNode(this.domElement);
   }
 
-  //protected get dataVersion(): Version {
-  //  return Version.parse('1.0');
-  //}
-
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     if (this.properties.includeCaption) {
       this.properties.includeCaption = true;
-    }
-    else {
+    } else {
       this.properties.includeCaption = false;
     }
     return {
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('title', {
+                PropertyPaneTextField("title", {
                   label: strings.TitleLabel,
                   value: this.properties.title,
                 }),
-                PropertyPaneTextField('siteUrl', {
+                PropertyPaneTextField("siteUrl", {
                   label: strings.SiteUrlFieldLabel,
                   onGetErrorMessage: this.onSiteUrlGetErrorMessage.bind(this),
                   value: this.context.pageContext.site.absoluteUrl,
                   deferredValidationTime: 1200,
                 }),
-                PropertyPaneDropdown('list', {
+                PropertyPaneDropdown("list", {
                   label: strings.ListFieldLabel,
                   options: this.lists,
                   disabled: this.listsDropdownDisabled,
                 }),
-                PropertyFieldNumber('numberImages', {
+                PropertyFieldNumber("numberImages", {
                   key: "numberValue",
                   label: "Number of images to load",
                   description: "Number between 1 and 250",
                   value: this.properties.numberImages,
                   maxValue: 250,
                   minValue: 1,
-                  disabled: false
+                  disabled: false,
                 }),
-                PropertyFieldNumber('sliderDelay', {
+                PropertyFieldNumber("sliderDelay", {
                   key: "delayValue",
                   label: "Delay in seconds until next image displays",
                   description: "Number between 1 and 60",
                   value: this.properties.sliderDelay,
                   maxValue: 60,
                   minValue: 1,
-                  disabled: false
+                  disabled: false,
                 }),
-                PropertyPaneDropdown('imageFitStyle', {
+                PropertyPaneDropdown("imageFitStyle", {
                   options: [
-                    {key: ImageFit.center, text: "center"}, 
-                    {key: ImageFit.centerContain, text: "centerContain"},
-                    {key: ImageFit.centerCover, text: "centerCover"},
-                    {key: ImageFit.contain, text: "contain"},
-                    {key: ImageFit.cover, text: "cover"},
-                    {key: ImageFit.none, text: "None"}
+                    { key: ImageFit.center, text: "center" },
+                    { key: ImageFit.centerContain, text: "centerContain" },
+                    { key: ImageFit.centerCover, text: "centerCover" },
+                    { key: ImageFit.contain, text: "contain" },
+                    { key: ImageFit.cover, text: "cover" },
+                    { key: ImageFit.none, text: "None" },
                   ],
                   label: "Image Fit Option",
-                  selectedKey: this.properties.imageFitStyle
+                  selectedKey: this.properties.imageFitStyle,
                 }),
-                PropertyPaneToggle('includeCaption', {
-                  label: "Include Caption"
+                PropertyPaneToggle("includeCaption", {
+                  label: "Include Caption",
                 }),
-                PropertyPaneLabel('errorMessage', {
-                  text:  this.errorMessage,
+                PropertyPaneLabel("errorMessage", {
+                  text: this.errorMessage,
                 }),
-              ]
-            }
-          ]
-        }
-      ]
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
--- samples/react-carousel/src/webparts/carousel/components/Carousel.tsx
+++ samples/react-carousel/src/webparts/carousel/components/Carousel.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import styles from './Carousel.module.scss';
-import { ICarouselProps } from './ICarouselProps';
-import { ICarouselState } from './ICarouselState';
-import spservices from '../../../spservices/spservices';
-import * as microsoftTeams from '@microsoft/teams-js';
-import { ICarouselImages } from './ICarouselmages';
-import 'video-react/dist/video-react.css'; // import css
-import  { Player, BigPlayButton } from 'video-react';
-import  Slider, { LazyLoadTypes } from "react-slick";
+import * as React from "react";
+import styles from "./Carousel.module.scss";
+import { ICarouselProps } from "./ICarouselProps";
+import { ICarouselState } from "./ICarouselState";
+import spservices from "../../../spservices/spservices";
+import * as microsoftTeams from "@microsoft/teams-js";
+import { ICarouselImages } from "./ICarouselmages";
+import "video-react/dist/video-react.css"; // import css
+import { Player, BigPlayButton } from "video-react";
+import Slider, { LazyLoadTypes } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import * as $ from 'jquery';
-import { FontSizes, } from '@uifabric/fluent-theme/lib/fluent/FluentType';
+import * as $ from "jquery";
+import { FontSizes } from "@uifabric/fluent-theme/lib/fluent/FluentType";
 import { Placeholder } from "@pnp/spfx-controls-react/lib/Placeholder";
-import * as strings from 'CarouselWebPartStrings';
-import { DisplayMode } from '@microsoft/sp-core-library';
-import { CommunicationColors } from '@uifabric/fluent-theme/lib/fluent/FluentColors';
+import * as strings from "CarouselWebPartStrings";
+import { DisplayMode } from "@microsoft/sp-core-library";
+import { CommunicationColors } from "@uifabric/fluent-theme/lib/fluent/FluentColors";
 import {
   Spinner,
   SpinnerSize,
@@ -24,148 +24,159 @@ import {
   Label,
   Icon,
   Image,
-  ImageLoadState, 
-  ImageFit
-} from 'office-ui-fabric-react';
+  ImageLoadState,
+  ImageFit,
+} from "office-ui-fabric-react";
 
-
-export default class Carousel extends React.Component<ICarouselProps, ICarouselState> {
+export default class Carousel extends React.Component<
+  ICarouselProps,
+  ICarouselState
+> {
   private spService: spservices = null;
   private _teamsContext: microsoftTeams.Context = null;
-
 
   public constructor(props: ICarouselProps) {
     super(props);
     this.spService = new spservices(this.props.context);
 
     if (this.props.context.microsoftTeams) {
-      this.props.context.microsoftTeams.getContext(context => {
+      this.props.context.microsoftTeams.getContext((context) => {
         this._teamsContext = context;
-        console.log('ctt', this._teamsContext.theme);
+        console.log("ctt", this._teamsContext.theme);
         this.setState({ teamsTheme: this._teamsContext.theme });
       });
-
     }
 
     this.state = {
       isLoading: false,
-      errorMessage: '',
+      errorMessage: "",
       hasError: false,
-      teamsTheme: 'default',
+      teamsTheme: "default",
       photoIndex: 0,
       carouselImages: [],
-      loadingImage: true
+      loadingImage: true,
     };
   }
-
 
   private onConfigure() {
     // Context of the web part
     this.props.context.propertyPane.open();
   }
 
-
   private async loadPictures() {
-
     this.setState({ isLoading: true, hasError: false });
     const tenantUrl = `https://${location.host}`;
     let galleryImages: ICarouselImages[] = [];
     let carouselImages: React.ReactElement<HTMLElement>[] = [];
 
     try {
-      const images = await this.spService.getImages(this.props.siteUrl, this.props.list, this.props.numberImages);
+      const images = await this.spService.getImages(
+        this.props.siteUrl,
+        this.props.list,
+        this.props.numberImages
+      );
 
       for (const image of images) {
-
         if (image.FileSystemObjectType == 1) continue; // by pass folder item
         const pURL = `${tenantUrl}/_api/v2.0/sharePoint:${image.File.ServerRelativeUrl}:/driveItem/thumbnails/0/large/content?preferNoRedirect=true `;
         const thumbnailUrl = `${tenantUrl}/_api/v2.0/sharePoint:${image.File.ServerRelativeUrl}:/driveItem/thumbnails/0/c240x240/content?preferNoRedirect=true `;
 
-        let mediaType: string = '';
+        let mediaType: string = "";
         switch (image.File_x0020_Type) {
-          case ('jpg' || 'jpeg' || 'png' || 'tiff' || 'gif'):
-            mediaType = 'image';
+          case "jpg" || "jpeg" || "png" || "tiff" || "gif":
+            mediaType = "image";
             break;
-          case ('mp4'):
-            mediaType = 'video';
+          case "mp4":
+            mediaType = "video";
             break;
           default:
             continue;
         }
 
-        galleryImages.push(
-          {
-            imageUrl: pURL,
-            mediaType: mediaType,
-            serverRelativeUrl: image.File.ServerRelativeUrl,
-            caption: image.Title ? image.Title : image.File.Name,
-            description: image.Description ? image.Description : '',
-            linkUrl: ''
-          },
-        );
+        galleryImages.push({
+          imageUrl: pURL,
+          mediaType: mediaType,
+          serverRelativeUrl: image.File.ServerRelativeUrl,
+          caption: image.Title ? image.Title : image.File.Name,
+          description: image.Description ? image.Description : "",
+          linkUrl: "",
+        });
 
         // Create Carousel Slides from Images
 
         carouselImages = galleryImages.map((galleryImage, i) => {
           return (
-            <div className='slideLoading' >
-
-              {galleryImage.mediaType == 'video' ?
-                <div >
+            <div className="slideLoading">
+              {galleryImage.mediaType == "video" ? (
+                <div>
                   <Player
                     poster={galleryImage.imageUrl}
-                    style={{ width: '100%', height: '400px' }}>
+                    style={{ width: "100%", height: "400px" }}
+                  >
                     <BigPlayButton position="center" />
-                    <source src={galleryImage.serverRelativeUrl}
-                    />
+                    <source src={galleryImage.serverRelativeUrl} />
                   </Player>
                 </div>
-                :
+              ) : (
                 <div>
-                  <Image src={galleryImage.imageUrl}
+                  <Image
+                    src={galleryImage.imageUrl}
                     onLoadingStateChange={async (loadState: ImageLoadState) => {
-                      console.log('imageload Status ' + i, loadState, galleryImage.imageUrl);
+                      console.log(
+                        "imageload Status " + i,
+                        loadState,
+                        galleryImage.imageUrl
+                      );
                       if (loadState == ImageLoadState.loaded) {
                         this.setState({ loadingImage: false });
                       }
                     }}
-                    height={'400px'}
+                    height={"400px"}
                     imageFit={this.props.imageFitStyle}
                   />
-                  {this.props.includeCaption == true ?
-                   <div className={styles.caption}>
-                    <h2 style={{fontSize: FontSizes.size20, textTransform: 'uppercase', color: 'white' }}>{galleryImage.caption}</h2>
-                    <p>{galleryImage.description}</p>
-                  </div>
-                  :
-                  ''}
+                  {this.props.includeCaption == true ? (
+                    <div className={styles.caption}>
+                      <h2
+                        style={{
+                          fontSize: FontSizes.size20,
+                          textTransform: "uppercase",
+                          color: "white",
+                        }}
+                      >
+                        {galleryImage.caption}
+                      </h2>
+                      <p>{galleryImage.description}</p>
+                    </div>
+                  ) : null}
                 </div>
-              }
+              )}
             </div>
           );
-        }
-        );
+        });
 
         this.setState({ carouselImages: carouselImages, isLoading: false });
       }
     } catch (error) {
-      this.setState({ hasError: true, errorMessage: decodeURIComponent(error.message) });
+      this.setState({
+        hasError: true,
+        errorMessage: decodeURIComponent(error.message),
+      });
     }
   }
 
-  public async  componentDidMount() {
+  public async componentDidMount() {
     await this.loadPictures();
   }
 
   public async componentDidUpdate(prevProps: ICarouselProps) {
-
     if (!this.props.list || !this.props.siteUrl) return;
     // Get  Properties change
-    if (prevProps.list !== this.props.list 
-      || prevProps.numberImages !== this.props.numberImages 
-      || prevProps.includeCaption !== this.props.includeCaption
-      || prevProps.imageFitStyle !== this.props.imageFitStyle)
-    {
+    if (
+      prevProps.list !== this.props.list ||
+      prevProps.numberImages !== this.props.numberImages ||
+      prevProps.includeCaption !== this.props.includeCaption ||
+      prevProps.imageFitStyle !== this.props.imageFitStyle
+    ) {
       /*
        this.galleryImages = [];
        this._carouselImages = [];
@@ -195,53 +206,68 @@ export default class Carousel extends React.Component<ICarouselProps, ICarouselS
 
     return (
       <div className={styles.carousel}>
-        <div>
-        </div>
-        {
-          (!this.props.list) ?
-            <Placeholder iconName='Edit'
-              iconText={strings.WebpartConfigIconText}
-              description={strings.WebpartConfigDescription}
-              buttonLabel={strings.WebPartConfigButtonLabel}
-              hideButton={this.props.displayMode === DisplayMode.Read}
-              onConfigure={this.onConfigure.bind(this)} />
-            :
-            this.state.hasError ?
-              <MessageBar messageBarType={MessageBarType.error}>
-                {this.state.errorMessage}
-              </MessageBar>
-              :
-              this.state.isLoading ?
-                <Spinner size={SpinnerSize.large} label='loading images...' />
-                :
-                this.state.carouselImages.length == 0 ?
-                  <div style={{ width: '300px', margin: 'auto' }}>
-                    <Icon iconName="PhotoCollection"
-                      style={{ fontSize: '250px', color: '#d9d9d9' }} />
-                    <Label style={{ width: '250px', margin: 'auto', fontSize: FontSizes.size20 }}>No images in the library</Label>
-                  </div>
-                  :
-                  <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
-
-                    <div style={{ width: '100%'}}>
-                      <Slider
-                        {...sliderSettings}
-                        autoplay={true}
-                        onReInit={() => {
-                          if (!this.state.loadingImage)
-                            $(".slideLoading").removeClass("slideLoading");
-                        }}>
-                        {
-                          this.state.carouselImages
-                        }
-                      </Slider>
-                    </div>
-                    {
-                      this.state.loadingImage &&
-                      <Spinner size={SpinnerSize.small} label={'Loading...'} style={{verticalAlign:'middle', right:'30%', top:20, position: 'absolute', fontSize: FontSizes.size18, color: CommunicationColors.primary }}></Spinner>
-                    }
-                  </div>
-        }
+        <div></div>
+        {!this.props.list ? (
+          <Placeholder
+            iconName="Edit"
+            iconText={strings.WebpartConfigIconText}
+            description={strings.WebpartConfigDescription}
+            buttonLabel={strings.WebPartConfigButtonLabel}
+            hideButton={this.props.displayMode === DisplayMode.Read}
+            onConfigure={this.onConfigure.bind(this)}
+          />
+        ) : this.state.hasError ? (
+          <MessageBar messageBarType={MessageBarType.error}>
+            {this.state.errorMessage}
+          </MessageBar>
+        ) : this.state.isLoading ? (
+          <Spinner size={SpinnerSize.large} label="loading images..." />
+        ) : this.state.carouselImages.length == 0 ? (
+          <div style={{ width: "300px", margin: "auto" }}>
+            <Icon
+              iconName="PhotoCollection"
+              style={{ fontSize: "250px", color: "#d9d9d9" }}
+            />
+            <Label
+              style={{
+                width: "250px",
+                margin: "auto",
+                fontSize: FontSizes.size20,
+              }}
+            >
+              No images in the library
+            </Label>
+          </div>
+        ) : (
+          <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+            <div style={{ width: "100%" }}>
+              <Slider
+                {...sliderSettings}
+                autoplay={true}
+                onReInit={() => {
+                  if (!this.state.loadingImage)
+                    $(".slideLoading").removeClass("slideLoading");
+                }}
+              >
+                {this.state.carouselImages}
+              </Slider>
+            </div>
+            {this.state.loadingImage && (
+              <Spinner
+                size={SpinnerSize.small}
+                label={"Loading..."}
+                style={{
+                  verticalAlign: "middle",
+                  right: "30%",
+                  top: 20,
+                  position: "absolute",
+                  fontSize: FontSizes.size18,
+                  color: CommunicationColors.primary,
+                }}
+              ></Spinner>
+            )}
+          </div>
+        )}
       </div>
     );
   }
